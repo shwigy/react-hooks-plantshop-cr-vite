b64c0c3194945fe3482db6db0b2fad0ef0d038e7
shwigy <floressj04@icloud.com>
2026-07-18 02:46:55 -0400

    add content to PlantCard.jsx

--- src/components/PlantCard.jsx
+++ src/components/PlantCard.jsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
-function PlantCard() {
+function PlantCard({ plant }) {
+  const [isInStock, setIsInStock] = useState(true);
+
+  function handleStockClick() {
+    setIsInStock((prev) => !prev);
+  }
+
   return (
     <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      <img src={plant.image} alt={plant.name} />
+      <h4>{plant.name}</h4>
+      <p>Price: {plant.price}</p>
+      {isInStock ? (
+        <button className="primary" onClick={handleStockClick}>
+          In Stock
+        </button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick={handleStockClick}>Out of Stock</button>
       )}
     </li>
   );
